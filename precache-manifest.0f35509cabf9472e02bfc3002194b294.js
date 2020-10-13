131634db07a0641b676c92a192ab20ab32269f32
namnguyen191 <hoangnamnguyen191@gmail.com>
2020-10-13 09:25:19 -0400

    Add a React project for testing

---
+++ precache-manifest.0f35509cabf9472e02bfc3002194b294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e723b7409972eeb09c57322e364db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad84e30294194a18105",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "aad84e30294194a18105",
+    "url": "/static/js/2.267dff26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.267dff26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620a75d4ed6852208426",
+    "url": "/static/js/main.99820530.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
