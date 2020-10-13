63aacecf78287c18ff52bd976d2a9d191114b951
namnguyen191 <hoangnamnguyen191@gmail.com>
2020-10-13 09:37:56 -0400

    Add another React project to test routing

---
+++ precache-manifest.1701b41a67b427181d1aff6f0f31e05d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a5232dd1135f390dafeb4ac2f6da89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd68390063e0cea8368",
+    "url": "/static/js/2.ba18f6ce.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/static/js/2.ba18f6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8823a25524908f7ac9",
+    "url": "/static/js/main.53ec0ceb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
